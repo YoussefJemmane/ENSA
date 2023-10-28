477357f2d4335253cdb6eccf3eaf957b9e17429b
YoussefJemmane <yous.jemm@gmail.com>
2023-10-28 12:26:19 +0200

    Day2

---
+++ Python/TPs/EX1_TP2.py
@@ -0,0 +1,9 @@
+a = int(input("Saisi numero 1"))
+b = int(input("Saisi numero 2"))
+
+if a*b > 0:
+    print("Le produit est positif")
+elif a*b < 0:
+    print("Le produit est negatif")
+else:
+    print("Le produit est nul")
